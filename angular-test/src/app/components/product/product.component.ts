72ed07e760d6303b76d115da2732c8c51f8afa4b
Vanny CHAN <vanny.chan@CKSNTRANSPORT.COM>
2024-08-12 16:41:19 +0700

    push code on crud

--- angular-test/src/app/components/product/product.component.ts
+++ angular-test/src/app/components/product/product.component.ts
@@ -1,19 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
 
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  constructor(private _snackBar: MatSnackBar) {}
+
   @ViewChild('myModal') model : ElementRef | undefined;
 
   productObj: products = new products();
+  productList: products[] = [];
+
+  ngOnInit(): void {
+    const LocalData = localStorage.getItem("products");
+    if(LocalData != null) {
+      this.productList = JSON.parse(LocalData);
+    }
+  }
 
   // Open dialog
   openModel() {
@@ -25,9 +49,21 @@ export class ProductComponent {
   
   // Close dialog
   closeModal(){
+    this.productObj = new products();
     if(this.model != null){
       this.model.nativeElement.style.display = "none";
     }
+  }
+
+  updateProduct(){
+    const currentProduct = this.productList.find(m=>m.id === this.productObj.id);
+    if(currentProduct!= undefined){
+      currentProduct.name = this.productObj.name;
+      currentProduct.price = this.productObj.price;
+      currentProduct.description = this.productObj.description;
+    };
+    localStorage.setItem('products', JSON.stringify(this.productList));
+    this.closeModal();
   }
 
   saveProduct(){
@@ -36,28 +72,62 @@ export class ProductComponent {
 
     if(isLocalPresent != null){
       const oldArray = JSON.parse(isLocalPresent);
+
+      this.productObj.id = oldArray.length +1;
+
       oldArray.push(this.productObj);
+
+      this.productList = oldArray;
+
       localStorage.setItem("products",JSON.stringify(oldArray));
     }
 
     else{
       const newArr = [];
       newArr.push(this.productObj);
+      this.productObj.id = 1;
+      this.productList = newArr;
       localStorage.setItem("products",JSON.stringify(newArr));
     }
+
+    // Close dialog after create
+    this.closeModal();
+    this.productObj = new products();
     
   }
+
+  // edit product
+  editProduct(item: products){
+    this.productObj = item;
+    this.openModel();   
+  }
+
+  // delete product
+  deleteProduct(){
+    const index = this.productList.findIndex((m: any) => m.name == this.productObj.name);
+    this.productList.splice(index, 1);
+    localStorage.setItem("products",JSON.stringify(this.productList));
+    this.closeModal();
+
+    this._snackBar.open('Delete success', 'Thanks', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  } 
 }
 
-
 export class products {
+  id: number;
   name: string;
   image: string;
+  price: string;
   description: string;
 
   constructor() {
+    this.id = 0;
     this.name = '';
     this.image = '';
+    this.price = '';
     this.description = '';
   }
 }
